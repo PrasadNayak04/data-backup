921f568187ef514883b0df98456abc3e5a79985b
PrasadNayak04 <111737831+PrasadNayak04@users.noreply.github.com>
2023-01-09 10:56:36 +0530

    Add files via upload

--- microservices/department_service/src/main/java/com/robosoft/department_service/controller/DepartmentController.java
+++ microservices/department_service/src/main/java/com/robosoft/department_service/controller/DepartmentController.java
@@ -2,12 +2,14 @@ package com.robosoft.department_service.controller;
 
 import com.robosoft.department_service.entity.Department;
 import com.robosoft.department_service.service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @EnableEurekaClient
+@Slf4j
 @RequestMapping("/department")
 public class DepartmentController {
 
@@ -16,11 +18,13 @@ public class DepartmentController {
 
     @PostMapping("/")
     public Department addDepartment(@RequestBody Department department){
+        log.info("inside add department");
         return departmentService.addDepartment(department);
     }
 
     @GetMapping("/{id}")
     public Department getDepartmentById(@PathVariable("id") Long departmentId){
+        log.info("inside find department by id");
         return departmentService.getDepartmentById(departmentId);
     }
 
--- microservices/user_service/src/main/java/com/robosoft/user_service/controller/UserController.java
+++ microservices/user_service/src/main/java/com/robosoft/user_service/controller/UserController.java
@@ -3,12 +3,14 @@ package com.robosoft.user_service.controller;
 import com.robosoft.user_service.VO.ResponseTemplate;
 import com.robosoft.user_service.entity.User;
 import com.robosoft.user_service.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @EnableEurekaClient
+@Slf4j
 @RequestMapping("/user")
 public class UserController {
 
@@ -17,11 +19,13 @@ public class UserController {
 
     @PostMapping("/")
     public User addDepartment(@RequestBody User department){
+        log.info("inside add user");
         return userService.addUser(department);
     }
 
     @GetMapping("/{id}")
     public ResponseTemplate getDepartmentById(@PathVariable("id") Long userId){
+        log.info("inside find user with department");
         return userService.getUserDetailsByUserId(userId);
     }
 
